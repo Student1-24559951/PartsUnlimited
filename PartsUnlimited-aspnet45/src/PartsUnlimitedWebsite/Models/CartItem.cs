8cc918d64f1dc491250339a6981ab7c6fed0aa77
Student1-24559951 <Student1-24559951@lodsasdoutlook.onmicrosoft.com>
2022-07-14 02:54:02 -0700

    My commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My secound change
+        //My First change
         [Key]
         public int CartItemId { get; set; }
 
